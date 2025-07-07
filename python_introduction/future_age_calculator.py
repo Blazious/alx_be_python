83d78a1616032a12d91b25c8bf26763390e2b358
Blazious <blaziousmwambuwa@gmail.com>
2025-07-07 09:42:01 +0300

    first commit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = int(input("How old are you ?"))
+
+years_to_add = 2050 - 2023 
+future_age = years_to_add + current_age
+
+#print results 
+print(f"You will be {future_age} years old in 2050")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+#define variables 
+hours = 2
+seconds = hours * 3600
+
+#print out results 
+print(f"number of seconds for {hours} is {seconds}")
